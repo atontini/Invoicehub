3f2807f0d39d7a851ead9f515249da7cf42a37eb
Andrea Tontini <andrea.tontini@outlook.com>
2024-12-20 12:30:16 +0100

    Categories template

--- backend/auth.py
+++ backend/auth.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, login_required, logout_user
-from .models import User, Product
+from .models import User, Product, Category
 from . import db
 from itsdangerous import URLSafeTimedSerializer
 from flask import current_app
@@ -98,3 +98,10 @@ def products():
     #items: id, first, last, email, phone, location, hobby
     products = Product.query.all()
     return render_template('products.html', products=products)
+
+@auth.route('/categories')
+@login_required
+def categories():
+    #items: id, first, last, email, phone, location, hobby
+    categories = Category.query.all()
+    return render_template('categories.html', categories=categories)
