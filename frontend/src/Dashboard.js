6dde7d11137cc8b90b2f0ebb80dc27653f3ea11e
Andrea Tontini <andrea.tontini@outlook.com>
2024-12-31 00:21:50 +0100

    Pie chart

--- frontend/src/Dashboard.js
+++ frontend/src/Dashboard.js
@@ -1,13 +1,35 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useAuth } from "./AuthContext";
-import { Line } from 'react-chartjs-2';
-import { Chart as ChartJS, Title, Tooltip, Legend, LineElement, CategoryScale, LinearScale, PointElement } from 'chart.js';
+import {
+  Chart as ChartJS,
+  ArcElement,
+  Tooltip,
+  Legend,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+} from 'chart.js';
+import { Line, Pie } from 'react-chartjs-2';
 
-ChartJS.register(Title, Tooltip, Legend, LineElement, CategoryScale, LinearScale, PointElement);
+ChartJS.register(
+  ArcElement,
+  Tooltip,
+  Legend,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title
+);
+
 
 const Dashboard = () => {
   const [chartData, setChartData] = useState(null);
+  const [pieChartData, setPieChartData] = useState(null);
+
   const { user } = useAuth();
 
   useEffect(() => {
@@ -39,6 +61,35 @@ const Dashboard = () => {
             },
           ],
         });
+
+        //Pie Chart Data
+        const labels1 = response.data.purchases_per_product.map(item => item.name);
+        const quantities1 = response.data.purchases_per_product.map(item => item.total_quantity);
+
+        setPieChartData({
+          labels: labels1,
+            datasets: [
+            {
+              label: "Products",
+              data: quantities1,
+              backgroundColor: [
+                "rgba(255, 99, 132, 0.6)",
+                "rgba(54, 162, 235, 0.6)",
+                "rgba(255, 206, 86, 0.6)",
+                "rgba(75, 192, 192, 0.6)",
+                "rgba(153, 102, 255, 0.6)",
+              ],
+              borderColor: [
+                "rgba(255, 99, 132, 1)",
+                "rgba(54, 162, 235, 1)",
+                "rgba(255, 206, 86, 1)",
+                "rgba(75, 192, 192, 1)",
+                "rgba(153, 102, 255, 1)",
+              ],
+              borderWidth: 1,
+            },
+          ],
+        });
       } catch (err) {
         console.error('Error fetching purchases data:', err);
       }
@@ -47,14 +98,23 @@ const Dashboard = () => {
     fetchAnalytics();
   }, []);
 
-  if (!chartData) {
+  if (!chartData && !pieChartData) {
     return <div className="text-center text-gray-500">Loading chart...</div>;
   }
 
   return (
     <div className="container mx-auto p-4">
-      <h2 className="text-xl font-bold mb-4 text-center">Purchases by Day and Hour</h2>
-      <Line data={chartData} />
+      <h1 className="text-xl font-bold mb-6 text-center">Analytics</h1>
+      <div className="grid grid-cols-3 gap-4">
+        <div className="col-span-2 p-4">
+          <h2 className="text-xl font-bold mb-4 text-center">Purchases by day and hour</h2>
+          <Line data={chartData} />
+        </div>
+        <div className="col-span-1 p-4">
+          <h2 className="text-xl font-bold mb-4 text-center">Most purchased product by name</h2>
+          <Pie data={pieChartData} />
+        </div>
+      </div>
     </div>
   );
 };
