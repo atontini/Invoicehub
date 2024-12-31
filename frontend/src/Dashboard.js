4ee9537dad1057a49a8b2ddb3424e509dcfc1cc2
Andrea Tontini <andrea.tontini@outlook.com>
2024-12-31 03:32:49 +0100

    cards in the Dashboard

--- frontend/src/Dashboard.js
+++ frontend/src/Dashboard.js
@@ -29,6 +29,7 @@ ChartJS.register(
 const Dashboard = () => {
   const [chartData, setChartData] = useState(null);
   const [pieChartData, setPieChartData] = useState(null);
+  const [cardsData, setCardsData] = useState(null);
 
   const { user } = useAuth();
 
@@ -90,21 +91,54 @@ const Dashboard = () => {
             },
           ],
         });
+
+        setCardsData(response.data.cards_data);
       } catch (err) {
         console.error('Error fetching purchases data:', err);
       }
     };
 
     fetchAnalytics();
-  }, []);
+  }, [user.access_token]);
 
-  if (!chartData && !pieChartData) {
+  const InfoCard = ({ title, value, subtitle, change, isPositive, icon }) => {
+    return (
+      <div className="bg-white shadow-lg rounded-lg p-4 w-64">
+        <div className="text-gray-500 text-sm font-medium">{title}</div>
+        <div className="text-3xl font-bold text-gray-800 mt-2">{value}</div>
+        <div className="text-gray-500 text-sm mt-1">{subtitle}</div>
+        <div className="flex items-center mt-2">
+          <span className={`text-lg font-medium ${isPositive ? "text-green-500" : "text-red-500"}`}>
+            {isPositive ? "▲" : "▼"} {change}
+          </span>
+        </div>
+      </div>
+    );
+  };
+
+  if (!chartData && !pieChartData && !cardsData) {
     return <div className="text-center text-gray-500">Loading chart...</div>;
   }
 
   return (
+    <>
+    <h1 className="text-xl font-bold mb-6 text-center">Analytics</h1>
+    <div className="flex flex-col items-center justify-center">
+      <div className="flex space-x-4">
+        {cardsData.map((card, index) => (
+          <InfoCard
+            key={index}
+            title={card.title}
+            value={card.value}
+            subtitle={card.subtitle}
+            change={card.change}
+            isPositive={card.isPositive}
+            icon={card.icon}
+          />
+        ))}
+      </div>
+    </div>
     <div className="container mx-auto p-4">
-      <h1 className="text-xl font-bold mb-6 text-center">Analytics</h1>
       <div className="grid grid-cols-3 gap-4">
         <div className="col-span-2 p-4">
           <h2 className="text-xl font-bold mb-4 text-center">Purchases by day and hour</h2>
@@ -116,6 +150,7 @@ const Dashboard = () => {
         </div>
       </div>
     </div>
+    </>
   );
 };
 
